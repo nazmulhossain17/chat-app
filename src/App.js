1871f2fc32879fd1f341db1f3a21b94e06c1be52
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-04-28 22:24:48 +0600

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Register from './Pages/Register/Register';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Register/>
+      {/* <Login/> */}
     </div>
   );
 }
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <>
+            <div className="bg-gray-200 min-h-screen flex flex-col">
+      <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+        <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+          <h1 className="mb-8 text-3xl text-center">Sign In</h1>
+
+          <input 
+            type="text"
+            className="block border border-gray-300 w-full p-3 rounded mb-4"
+            name="email"
+            placeholder="Email" />
+
+          <input 
+            type="password"
+            className="block border border-gray-300 w-full p-3 rounded mb-4"
+            name="password"
+            placeholder="Password" />
+
+          <button
+            type="submit"
+            className="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-700 focus:outline-none my-1"
+          >
+            Create Account
+          </button>
+
+          <div className="text-center text-sm text-gray-500 mt-4">
+            By signing up, you agree to the{' '}
+            <a className="no-underline border-b border-gray-500 text-gray-500" href="#">
+              Terms of Service
+            </a>{' '}
+            and{' '}
+            <a className="no-underline border-b border-gray-500 text-gray-500" href="#">
+              Privacy Policy
+            </a>
+          </div>
+        </div>
+
+        <div className="text-gray-500 mt-6">
+          Don't have an account?{' '}
+          <a className="no-underline border-b border-blue-500 text-blue-500" href="../login/">
+            Sign up
+          </a>
+          .
+        </div>
+      </div>
+    </div>
+        </>
+    );
+};
+
+export default Login;
